c3685676de8ee8703b1a925c926d477ec41b14e6
karenhirayama <karenm.hirayama@gmail.com>
2025-12-02 15:59:41 -0300

    feat: Update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,24 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+import { AiService } from './ai/ai.service';
+import { AiModule } from './ai/ai.module';
+import { ConfigModule } from '@nestjs/config';
+import { FriendshipController } from './friendship/friendship.controller';
+import { FriendshipService } from './friendship/friendship.service';
+import { FriendshipModule } from './friendship/friendship.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule, ChatModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    ChatModule,
+    AiModule,
+    FriendshipModule,
+  ],
+  controllers: [AppController, FriendshipController],
+  providers: [AppService, AiService, FriendshipService],
 })
 export class AppModule {}
